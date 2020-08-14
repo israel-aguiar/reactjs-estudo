45c96ec59886e219acf323e406f341c233936606
Israel <israel.aguiar@gmail.com>
2020-08-13 22:47:47 -0300

    #07 ReactJS para Iniciantes - Implementando um formulário de cadastro

--- web/src/components/Promotion/List/List.js
+++ web/src/components/Promotion/List/List.js
@@ -10,7 +10,7 @@ const PromotionList = ({ loading, promotions}) => {
     return (
         <div className="promotion-list">
             {promotions.map((promotion) => (
-                <PromotionCard promotion={promotion}></PromotionCard>
+                <PromotionCard key={promotion.id} promotion={promotion}></PromotionCard>
             ))}
         </div>
     )
