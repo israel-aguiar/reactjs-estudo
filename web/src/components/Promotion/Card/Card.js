5a9a9e00592628ca4f7e3d19753b9d3699d5a985
Israel <israel.aguiar@gmail.com>
2020-08-19 13:02:36 -0300

    #11 ReactJS para Iniciantes - Como implementar um Modal com createPortal

--- web/src/components/Promotion/Card/Card.js
+++ web/src/components/Promotion/Card/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import "./Card.css";
 
-const PromotionCard = ({ promotion }) => (
+const PromotionCard = ({ promotion, onClickComments }) => (
   <div className="promotion-card">
     <img src={promotion.imageUrl} alt={promotion.title} className="promotion-card__image" />
     <div className="promotion-card__info">
@@ -14,19 +14,21 @@ const PromotionCard = ({ promotion }) => (
             "{promotion.comments[0].comment}"
           </div>
         )}
-        <div className="promotion-card__comments-count">
+        <button className="promotion-card__comments-count" onClick={onClickComments}>
           {promotion.comments.length}{" "}
           {promotion.comments.length > 1 ? "comentários" : "comentário"}
-        </div>
+        </button>
         <a
           href={promotion.url}
           target="_blank"
           rel="noopener noreferrer"
           className="promotion-card__link"
         >
-          IR PARA O SITE
+          Ir Para o Site
         </a>
-        <Link to={`/edit/${promotion.id}`}>Editar</Link>
+        <Link to={`/edit/${promotion.id}`} className="promotion-card__edit-button">
+          Editar
+        </Link>
       </footer>
     </div>
   </div>
